259b10597c31c3b1a3e4f59bd241d5c29d448a75
cheoluk <cheoluk96@hanmail.net>
2024-12-12 22:24:05 +0900

    서버 배포용도 커밋3

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+//const { defineConfig } = require('@vue/cli-service')
+/* module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production' ? '/vuejs-todo/' : '/'
-})
+}) */
+
+  module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/vuejs-todo/' : '/'
+};
